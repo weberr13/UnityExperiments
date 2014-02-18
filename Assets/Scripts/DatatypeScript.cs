af7e02d37c750574758253068d6a0efe3970111b
Robert Weber <weberr13@gmail.com>
2014-02-17 21:13:16 -0700

    useful comments

--- Assets/Scripts/DatatypeScript.cs
+++ Assets/Scripts/DatatypeScript.cs
@@ -6,11 +6,11 @@ public class DatatypeScript : MonoBehaviour
 
    void Start ()
    {
-      //Value type variable
+      //Value type variable (int, float, double, bool, char, Vector3, Quaternion)
       Vector3 pos = transform.position;
       pos = new Vector3 (0, 2, 0);
 
-      //Reference type variable
+      //Reference type variable (Classes: Transform, GameObject, etc)
       Transform tran = transform;
       tran.position = new Vector3 (0, 3, 0);
    }
