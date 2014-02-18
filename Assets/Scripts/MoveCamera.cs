603bbbb921d5d24f09c20adfbeb39a361c6cad81
Robert Weber <weberr13@gmail.com>
2014-02-17 20:03:31 -0700

    Zooming camera, with limits

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class MoveCamera : MonoBehaviour
 {
    public float speed = 20;
+   public float personalSpace = 5;
    private Vector3 startPosition3;
+   public Transform target;
+
    // Use this for initialization
    void Start ()
    {
@@ -14,12 +17,18 @@ public class MoveCamera : MonoBehaviour
    // Update is called once per frame
    void Update ()
    {
+      transform.LookAt (target);
       float vertValue = Input.GetAxis ("CameraV");
       float horValue = Input.GetAxis ("CameraH");
-
+      float zoomValue = Input.GetAxis ("Zoom");
       if (Input.GetButtonDown ("ResetCamera")) {
          transform.position = startPosition3;
       } else {
+         if (zoomValue != 0) {
+            if (((target.position - transform.position).magnitude > personalSpace) || zoomValue < 0) {
+               transform.Translate (Vector3.forward * zoomValue * speed * Time.deltaTime);
+            }
+         }
          if (vertValue != 0) {
             transform.Translate (Vector3.up * vertValue * speed * Time.deltaTime);
          }
