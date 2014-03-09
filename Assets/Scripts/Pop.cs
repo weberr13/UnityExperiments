e5d6c361731db85b2377cdbed691224acf3ee699
Robert Weber <weberr13@gmail.com>
2014-03-08 17:18:09 -0700

    Physics triggers

---
+++ Assets/Scripts/Pop.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pop : MonoBehaviour
+{
+
+   // Use this for initialization
+   void Start ()
+   {
+
+   }
+	
+   void OnTriggerEnter (Collider other)
+   {
+      if (!other.isTrigger) {
+         Destroy (gameObject);
+      }
+   }
+
+}
